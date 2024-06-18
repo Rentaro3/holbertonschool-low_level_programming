cf164eafb25bbaa4c88fc93d863c1882734a3ae6
Rene Alsina <renealsina13@gmail.com>
2024-06-18 10:44:00 -0400

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+
+int main() {
+    // Loop through numbers 0 to 9
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", i);  // Print the number followed by a new line
+    }
+
+    return 0;
+}
+
