597944b2a45e98d76530ce167f55563429f997f9
Rene Alsina <renealsina13@gmail.com>
2024-06-18 12:57:27 -0400

    9-6

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,25 +1,17 @@
-#include <stdio.h>
-
-/**
- * main - Prints all possible combinations of single-digit numbers.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int num;
-
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
-
-	putchar('\n');
-
-	return (0);
-}
+#include <stdio.h> 
+ 
+int main() { 
+    int i, j; 
+ 
+    for (i = 0; i <= 9; i++) { 
+        for (j = 0; j <= 9; j++) { 
+            if (i != j) { // check if digits are different 
+                putchar(i + '0'); 
+                putchar(j + '0'); 
+                putchar(' '); 
+            } 
+        } 
+    } 
+ 
+    return 0; 
+} 
