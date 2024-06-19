a481a25d9b9df2c34c2bc02dc24945adc0b91e04
Rene Alsina <renealsina13@gmail.com>
2024-06-19 09:22:36 -0400

    0-7

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <_putchar>
+#include <unistd.h>
 
 /**
  * main - Prints "_putchar" followed by a new line.
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	printf('_');
-	printf('p');
-	printf('u');
-	printf('t');
-	printf('c');
-	printf('h');
-	printf('a');
-	printf('r');
-	printf('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
