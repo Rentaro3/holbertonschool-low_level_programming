d64fa37402b769c90eefd048e59b72cd4c9d253d
Rene Alsina <renealsina13@gmail.com>
2024-06-20 10:29:19 -0400

    3-2

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * print_numbers - Prints the numbers, from 0 to 9
- * /
+*/
 void print_numbers(void)
 {
-	int num
+	int num;
 
-	for (num =0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 		_putchar((num % 10) + '0');
 
-		_putchar('\n');
+	_putchar('\n');
 }
